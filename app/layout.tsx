5c94447f5166d51fd3a5360593f491d071351570
Devon Daviau <devondaviau@yahoo.ca>
2025-01-13 16:04:31 -0500

    formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,7 +100,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="dark:bg-squidInkPowder text-squidInkPowder bg-white pl-[calc(100vw-100%)] antialiased dark:text-white">
+      <body className="bg-white pl-[calc(100vw-100%)] text-squidInkPowder antialiased dark:bg-squidInkPowder dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
