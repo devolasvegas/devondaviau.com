9cb5444b1f5028b2f307185f578f636b3e540741
Devon Daviau <devondaviau@yahoo.ca>
2025-09-22 10:23:07 -0400

    swap in an environment variable for the Umami analytics domain string literals

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -35,7 +35,7 @@ const siteMetadata = {
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-      src: 'https://umami-production-2d0d.up.railway.app/script.js',
+      src: `https://${process.env.NEXT_UMAMI_DOMAIN}/script.js`,
     },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' umami-production-2d0d.up.railway.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' ${process.env.NEXT_UMAMI_DOMAIN};
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
